297166123b4fc514da9be7fd0a4ace26b3507076
lucarin91 <lucarin91@gmail.com>
2014-04-14 15:06:44 +0200

    sistemato jqueryuploader

--- php/style/template.php
+++ php/style/template.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>La tua Cassaforte</title>
-    <link rel="stylesheet" type="text/css" href="style/cassaforteStyle.css">
-    <link rel="stylesheet" type="text/css" href="style/templateStyle.css">
-    <link rel="stylesheet" type="text/css" href="style/getProStyle.css">
-    <link rel="stylesheet" type="text/css" href="style/listFile.css">
-    <link rel="stylesheet" type="text/css" href="style/slide.css">
+  <title>La tua Cassaforte</title>
+  <link rel="stylesheet" type="text/css" href="style/cassaforteStyle.css">
+  <link rel="stylesheet" type="text/css" href="style/templateStyle.css">
+  <link rel="stylesheet" type="text/css" href="style/getProStyle.css">
+  <link rel="stylesheet" type="text/css" href="style/listFile.css">
+  <link rel="stylesheet" type="text/css" href="style/slide.css">
 
-   	<script src="js/jquery-2.1.0.min.js"></script> 
-   	
-   	<link rel="stylesheet" type="text/css" href="style/jquery.svg.css"> 
-	<script type="text/javascript" src="js/jquery.svg.js"></script>
-	<script type="text/javascript" src="js/jquery.svgdom.js"></script>
-	
+ 	<script src="js/jquery-2.1.0.min.js"></script> 
+ 	
 	<link href="js/uploadFile/css/uploadfile.css" rel="stylesheet">
-	<script src="js/uploadFile/js/jquery.uploadfile.js"></script>
-
-
+	<script src="js/uploadFile/js/jquery.uploadfile.min.js"></script>
 </head>
 <body>
   <script>
